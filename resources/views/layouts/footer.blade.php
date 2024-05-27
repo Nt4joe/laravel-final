8d397c7d86da339176e89068c3fb41a1077f6493
Nt4joe <steamdota324@gmail.com>
2024-05-27 08:12:58 +0800

    Nice Admin successfully integrated!

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<footer id="footer" class="footer">
+    <div class="copyright">
+        &copy; Copyright <strong><span>Laravel Final</span></strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+        Modified by <a href="Hehe">John Lester Quiambao</a>
+    </div>
+</footer>
+<!-- End Footer -->
